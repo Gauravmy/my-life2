22dd381c9d5ae8293ac2f4f0c37b8ccd34f0ed6b
Gauravmy <153372786+Gauravmy@users.noreply.github.com>
2024-09-13 21:23:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Scroll to top button functionality
+document.getElementById('scrollBtn').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
